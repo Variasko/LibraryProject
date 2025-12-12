be22d07e13b18fb67ee6ead5b479a3d6e7f87de6
Variasko <121598389+Variasko@users.noreply.github.com>
2025-12-12 15:18:29 +0300

    реализация получения всех книг

---
+++ LibrarryApp/LibrarryDesktop/Infrastructure/Services/IBookService.cs
@@ -0,0 +1,9 @@
+﻿using LibraryDesktop.Models.ApiResponceModels;
+
+namespace LibraryDesktop.Infrastructure.Services
+{
+    public interface IBookService
+    {
+        Task<List<Book>> GetBooksAsync(CancellationToken cancellationToken = default);
+    }
+}
---
+++ LibrarryApp/LibrarryDesktop/Infrastructure/Services/Implementation/BookService.cs
@@ -0,0 +1,33 @@
+﻿using LibraryDesktop.Infrastructure.API;
+using LibraryDesktop.Models.ApiResponceModels;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace LibraryDesktop.Infrastructure.Services.Implementation
+{
+    public class BookService : IBookService
+    {
+        private readonly HttpClient _httpClient;
+
+        public BookService(HttpClient httpClient)
+        {
+            _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+        }
+
+        public async Task<List<Book>> GetBooksAsync(CancellationToken cancellationToken = default)
+        {
+            var response = await _httpClient.GetAsync(Routers.Books.GetBooks, cancellationToken);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonString = await response.Content.ReadAsStringAsync(cancellationToken);
+                var books = JsonConvert.DeserializeObject<List<Book>>(jsonString);
+                return books ?? new List<Book>();
+            }
+
+            
+            response.EnsureSuccessStatusCode();
+            return new List<Book>();
+        }
+    }
+}
